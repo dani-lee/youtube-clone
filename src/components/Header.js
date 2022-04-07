2019dc46198e970fb13d5ac9e5ac91445c770fad
dani-lee <ldb1213@gmail.com>
2022-04-07 16:57:12 +0900

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,45 @@
+import classes from "./Header.module.css";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { faVideo } from "@fortawesome/free-solid-svg-icons";
+import { faGripVertical } from "@fortawesome/free-solid-svg-icons";
+import { faBell } from "@fortawesome/free-regular-svg-icons";
+import { faCircleUser } from "@fortawesome/free-solid-svg-icons";
+
+const Header = () => {
+  return (
+    <div className={classes.header}>
+      <div className={classes["header__left"]}>
+        <FontAwesomeIcon icon={faBars} />
+        <img
+          className={classes["header__logo"]}
+          src="https://upload.wikimedia.org/wikipedia/commons/e/e1/Logo_of_YouTube_%282015-2017%29.svg"
+          alt="logo"
+        />
+      </div>
+      <div className={classes["header__input"]}>
+        <input type="text" placeholder="Search" />
+        <FontAwesomeIcon
+          icon={faMagnifyingGlass}
+          className={classes["header__inputButton"]}
+        />
+      </div>
+      <div className={classes["header__icons"]}>
+        <FontAwesomeIcon icon={faVideo} className={classes["header__icon"]} />
+        <FontAwesomeIcon
+          icon={faGripVertical}
+          className={classes["header__icon"]}
+        />
+        <FontAwesomeIcon icon={faBell} className={classes["header__icon"]} />
+        <FontAwesomeIcon
+          icon={faCircleUser}
+          className={classes["header__icon"]}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAzEeKhRjr3-edTiHrtm7XMoCrJyx4GspA",
+  authDomain: "clone-71395.firebaseapp.com",
+  projectId: "clone-71395",
+  storageBucket: "clone-71395.appspot.com",
+  messagingSenderId: "615730405863",
+  appId: "1:615730405863:web:aa0f8cd55bed69c71b5d42",
+};
